010091575610b7e2f4eff05bf7388019fb1e2a83
ind1010 <indup@princeton.edu>
2024-10-23 00:46:15 -0400

    added code for next and future, testing next

--- train.py
+++ train.py
@@ -105,7 +105,70 @@ class Meters:
     def avg(self, name):
         return self.meters[name].avg
 
-def train(cfg, policy_net, target_net, optimizer, batch, transform_fn, discount_factor):
+# def train(cfg, policy_net, target_net, optimizer, batch, transform_fn, discount_factor):
+#     state_batch = torch.cat([transform_fn(s) for s in batch.state]).to(device)  # (32, 4, 96, 96)
+#     action_batch = torch.tensor(batch.action, dtype=torch.long).to(device)  # (32,)
+#     reward_batch = torch.tensor(batch.reward, dtype=torch.float32).to(device)  # (32,)
+#     non_final_next_states = torch.cat([transform_fn(s) for s in batch.next_state if s is not None]).to(device, non_blocking=True)  # (<=32, 4, 96, 96)
+
+#     output = policy_net(state_batch)  # (32, 2, 96, 96)
+#     state_action_values = output.view(cfg.batch_size, -1).gather(1, action_batch.unsqueeze(1)).squeeze(1)  # (32,)
+#     next_state_values = torch.zeros(cfg.batch_size, dtype=torch.float32, device=device)  # (32,)
+#     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool, device=device)  # (32,)
+
+#     if cfg.use_double_dqn:
+#         with torch.no_grad():
+#             best_action = policy_net(non_final_next_states).view(non_final_next_states.size(0), -1).max(1)[1].view(non_final_next_states.size(0), 1)  # (<=32, 1)
+#             next_state_values[non_final_mask] = target_net(non_final_next_states).view(non_final_next_states.size(0), -1).gather(1, best_action).view(-1)  # (<=32,)
+#     else:
+#         next_state_values[non_final_mask] = target_net(non_final_next_states).view(non_final_next_states.size(0), -1).max(1)[0].detach()  # (<=32,)
+
+#     expected_state_action_values = (reward_batch + discount_factor * next_state_values)  # (32,)
+#     td_error = torch.abs(state_action_values - expected_state_action_values).detach()  # (32,)
+
+#     loss = smooth_l1_loss(state_action_values, expected_state_action_values)
+
+#     optimizer.zero_grad()
+#     loss.backward()
+#     if cfg.grad_norm_clipping is not None:
+#         torch.nn.utils.clip_grad_norm_(policy_net.parameters(), cfg.grad_norm_clipping)
+#     optimizer.step()
+
+#     train_info = {}
+#     train_info['td_error'] = td_error.mean().item()
+#     train_info['loss'] = loss.item()
+
+#     return train_info
+
+def train_next(cfg, policy_net, optimizer, batch, transform_fn, discount_factor):
+    state_batch = torch.cat([transform_fn(s) for s in batch.state]).to(device)  # (32, 4, 96, 96)
+    action_batch = torch.tensor(batch.action, dtype=torch.long).to(device)  # (32,)
+    reward_batch = torch.tensor(batch.reward, dtype=torch.float32).to(device)  # (32,)
+    non_final_next_states = torch.cat([transform_fn(s) for s in batch.next_state if s is not None]).to(device, non_blocking=True)  # (<=32, 4, 96, 96)
+
+    output = policy_net(state_batch)  # (32, 2, 96, 96)
+    state_action_values = output.view(cfg.batch_size, -1).gather(1, action_batch.unsqueeze(1)).squeeze(1)  # Q_next of (s,a)
+    next_state_values = torch.zeros(cfg.batch_size, dtype=torch.float32, device=device)  # (32,)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool, device=device)  # (32,)
+
+    expected_state_action_values = reward_batch # r_t
+    td_error = torch.abs(state_action_values - expected_state_action_values).detach()  # (32,)
+
+    loss = smooth_l1_loss(state_action_values, expected_state_action_values)
+
+    optimizer.zero_grad()
+    loss.backward()
+    if cfg.grad_norm_clipping is not None:
+        torch.nn.utils.clip_grad_norm_(policy_net.parameters(), cfg.grad_norm_clipping)
+    optimizer.step()
+
+    train_info = {}
+    train_info['td_error'] = td_error.mean().item()
+    train_info['loss'] = loss.item()
+
+    return train_info
+
+def train_future(cfg, policy_net, target_net, optimizer, batch, transform_fn, discount_factor):
     state_batch = torch.cat([transform_fn(s) for s in batch.state]).to(device)  # (32, 4, 96, 96)
     action_batch = torch.tensor(batch.action, dtype=torch.long).to(device)  # (32,)
     reward_batch = torch.tensor(batch.reward, dtype=torch.float32).to(device)  # (32,)
@@ -123,7 +186,7 @@ def train(cfg, policy_net, target_net, optimizer, batch, transform_fn, discount_
     else:
         next_state_values[non_final_mask] = target_net(non_final_next_states).view(non_final_next_states.size(0), -1).max(1)[0].detach()  # (<=32,)
 
-    expected_state_action_values = (reward_batch + discount_factor * next_state_values)  # (32,)
+    expected_state_action_values = (discount_factor * next_state_values)  # (32,)
     td_error = torch.abs(state_action_values - expected_state_action_values).detach()  # (32,)
 
     loss = smooth_l1_loss(state_action_values, expected_state_action_values)
@@ -257,7 +320,7 @@ def main(cfg):
             for i in range(num_robot_groups):
                 batch = replay_buffers[i].sample(cfg.batch_size) #CHANGED THIS
                 #batch = replay_buffers[i].sample(10)
-                train_info = train(cfg, policy.policy_nets[i], target_nets[i], optimizers[i], batch, policy.apply_transform, cfg.discount_factors[i])
+                train_info = train_next(cfg, policy.policy_nets[i], target_nets[i], optimizers[i], batch, policy.apply_transform, cfg.discount_factors[i])
 
                 if cfg.use_predicted_intention:
                     train_info_intention = train_intention(policy.intention_nets[i], optimizers_intention[i], batch, policy.apply_transform)
